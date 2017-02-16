2c267033d711c87cfb17b37a96a1dd24e4716fb3
Harendra56 <harendrasaini35@gmail.com>
2017-02-16 07:07:35 +0530

    Add files via upload

---
+++ action_page.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.CAEHS.COM">
+<html xmlns="http://www.CAEHS.COMhtml">
+  
+<head>
+<title>CAEHS</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<style type="text/css">
+  * {padding:0; margin:0;}
+  a {text-decoration:none;}
+  p {padding-left:10px;}
+  .container {width:700px; margin-left:auto; margin-right:auto;}
+  .header {background-color:#0000ff}
+  .header h1 {color:white; line-height:80px;}
+  .header img {float:left; height:70px; width:120px; padding:5px; margin-right:20px}
+  .nav {background-color:#ffdd99; width:150px; float:left;}
+  .nav ul {margin:20px; list-style-type:none;}
+  .content {float:left; width:400px;}
+  .right {background-color:#00f0f0; float:left;    width:150px;}
+ .footer {clear:left; background-color:#bb9955;}
+</style>
+</head>
+<body>
+ <div class="container">
+  <div class="header">
+   <img src="harendra.jpg" alt="CAEHS" />
+   <h1><h1 align=center>CAEHS<h1></h1></h1>
+</div>
+<div class="footer">
+ <span class="fsxl fcg"><form action="" method="get">
+            <input name="search" class="search" placeholder="Find Friends+Pages"><a href>Search</a><a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Logout</a></span><h1>
+
+</div>
+
+<div class="nav">
+   <p><strong>Harendra saini</strong></p>
+   <ul>
+    <b><u><li><a href="Profile.html">Profile</a></li></b></u>
+    <li><a href="Edit Profile.html">Edit               Profile</a></li>
+     <li><a href="Messages.html">Messages</a></li>
+    <li><u><a href="Games.html">Games</a></u></li>
+    <li><u><a href="Jobs.html">Jobs</a></u></li>
+    <li><u><a href="Pages.html">pages</a></u></li>
+<li><a href="create page.html">create page</a></li>
+<li><a href="Like pages.html">Like pages</a></li>
+<div class="CAEHS-page" 
+<li><a href="create Event.html">create Event</a></li>
+<li><a href="Event.html">Event</a></li>
+<li><a href="create ad.html">create ad</a></li>
+</ul>
+  </div>
+  <div class="Content">
+   <p><strong>Timeline</strong></p>
+   <p><img src="purple-pink-loading-gif-animation-large.gif"></p>
+ <!-- Trigger the Modal -->
+<img id="myImg" src="loading_logofinal_by_zegerdon-d60eb1v.gif" alt="Trolltunga, Norway" width="200" height="200">
+
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- The Close Button -->
+  <span class="close" onclick="document.getElementById('myModal').style.display='none'">&times;</span>
+
+  <!-- Modal Content (The Image) -->
+  <img class="modal-content" id="img01">
+
+  <!-- Modal Caption (Image Text) -->
+  <div id="caption"></div>
+</div>  
+   
+   </div>
+  
+<div class="footer">
+   <h1 align=center><span class="fsxl fcg">More Stories</span><h1>
+
+</div>
+
+ <!-- The overlay -->
+<div id="myNav" class="overlay">
+
+  <!-- Button to close the overlay navigation -->
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+
+  <!-- Overlay content -->
+  <div class="overlay-content">
+    <a href="#">About</a>
+    
+   <a href="#">Contact</a>
+  </div>
+
+</div>
+
+<!-- Use any element to open/show the overlay navigation menu -->
+<span onclick="openNav()"><h1 align=center>Harendrasaini35@gmail.com<h1></span>
+
+ 
+</body>
+</html>
